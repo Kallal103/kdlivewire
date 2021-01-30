6f99562bbbe30b71e6aeb55ee7f1e2306471f4d0
kallalnsu103 <kallal.das@northsouth.edu>
2021-01-30 16:21:31 +0600

    Mount event, fetch from DB

---
+++ app/Http/Livewire/Comments.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Comment;
+use Carbon\Carbon;
+use Livewire\Component;
+
+class Comments extends Component
+{
+    public $comments;
+
+    public $newComment;
+
+    public function mount()
+    {
+        $initialComments = Comment::all();
+        $this->comments = $initialComments;
+    }
+
+    public function addComment(){
+        if($this->newComment == ''){
+            return;
+        }
+        array_unshift($this->comments, [
+            'body' => $this->newComment,
+            'created_at' => Carbon::now()->diffForHumans(),
+            'creator' => 'GG Link'
+        ] );
+
+        $this->newComment = "";
+
+    }
+    public function render()
+    {
+        return view('livewire.comments');
+    }
+}
