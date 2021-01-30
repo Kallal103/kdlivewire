dadd1dec36f8b9d054e546e2396a3043d731d946
kallalnsu103 <kallal.das@northsouth.edu>
2021-01-30 22:17:55 +0600

    store comment with livewire

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -14,7 +14,7 @@ class Comments extends Component
 
     public function mount()
     {
-        $initialComments = Comment::all();
+        $initialComments = Comment::latest()->get();
         $this->comments = $initialComments;
     }
 
@@ -22,11 +22,10 @@ class Comments extends Component
         if($this->newComment == ''){
             return;
         }
-        array_unshift($this->comments, [
-            'body' => $this->newComment,
-            'created_at' => Carbon::now()->diffForHumans(),
-            'creator' => 'GG Link'
-        ] );
+        $createdComment = Comment::create([
+            'body'=> $this->newComment, 'user_id'=> 1
+            ]);
+        $this->comments->prepend($createdComment);
 
         $this->newComment = "";
 
